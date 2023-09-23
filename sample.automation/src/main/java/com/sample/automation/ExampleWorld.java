582e6c75e52876cd0eb68457f0ce190066415931
Rohit Dwivedi <rohit151092@gmail.com>
2023-09-23 23:41:18 +0530

    First commit C1

--- sample.automation/src/main/java/com/sample/automation/ExampleWorld.java
+++ sample.automation/src/main/java/com/sample/automation/ExampleWorld.java
@@ -4,5 +4,6 @@ public class ExampleWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!999999999999");
+		System.out.println("Hello Test");
 	}
 }
